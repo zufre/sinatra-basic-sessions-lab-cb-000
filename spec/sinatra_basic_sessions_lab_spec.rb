e89128cf4bc2d9470862d117ce8f830a7e3d9f26
vicfriedman <victoria.m.friedman@gmail.com>
2015-11-12 10:39:15 -0500

    tests

---
+++ spec/sinatra_basic_sessions_lab_spec.rb
@@ -0,0 +1,28 @@
+equire 'spec_helper'
+
+describe 'App' do
+  
+  describe "GET '/'" do
+    before(:each) do
+      get '/'
+    end
+    
+    it "returns a 200 status code" do
+      get '/'
+      expect(last_response.status).to eq(200)
+    end
+
+
+    it "sets session[:name] equal to your name" do
+      
+      expect(session.has_key?(:name)).to eq(true)
+    end
+
+
+   it "says Hello in the view" do
+      expect(last_response.body).to include("Hello, ")
+    end
+
+  end
+
+end
