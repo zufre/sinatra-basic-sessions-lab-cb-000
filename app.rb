51d882e4a7f1c18d6629af788a39aaf95f4094aa
Cedric David <cedric.david@btopenworld.com>
2018-09-20 06:38:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+  get '/' do
+
+    erb :index
+  end
+  post '/checkout' do
+    session[:item] = params[:item]
+    @cart =  session[:item]
+    erb :cart
+  end
+
 end
